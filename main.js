48203622d81f5015ca97e49089f524d78656e0cd
Sviatoslove <svyatpopov86@gmail.com>
2022-05-19 14:15:57 +0300

    f

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
-var flavors = [
-  'Краб', 
-  'Сметана и зелень', 
-  'Малосольные огурчики',
-  'Классический', 
-  'С сыром', 
-  'Паприка'
+
+
+/* 
+    TASK 3
+    
+    Есть список слов : [‘радар’,’вор’,’букет’,’доход’,’казак’,’варяг’,’потом’].
+    Найти среди них палиндромы.
+
+*/
+
+var words = [
+  'радар','вор','букет','доход','казак','варяг','потом'
 ];
 
-var x = 'Новый вкус';
-
-function flavorsNew(array) {
-  var messege;
-  for(i=0; i < array.length; i++) {
-    if(i == array.length) {
-      array.push(x);
-      messege = x;
+function palindromeSearch(array) {
+  for(i=0; i < array.length; i++){
+    function reverseString() {
+      return str.split("").reverse().join("");
     }
-    else {
-      messege = array[array.length - 1];
-    }
+    if(i = reverseString(i)) {
+      arrPalindrome += array.slice(i,(i + 1)) + ',';
+    } else {
+      arrPalindrome = [''];
+    };
   }
-  return messege;
+  return arrPalindrome;
 }
+var result = palindromeSearch(words);
+console.log(result);
 
-flavorsNew(flavors);
+
+
+
+/* 
+    TASK 4
+    
+    Есть массив чисел : [444,123,-114,231,775,221,766,-557,0]. 
+    Необходимо найти максимальное число и вывести его в консоль
+
+*/
+
+/* 
+    TASK 5
+    
+    Пользователь вводит многозначное число. 
+    Напишите функцию, которая принимает число в качестве аргумента
+    и вставляет двоеточие (:) между двумя нечетными числами. 
+    Например, если вводится число 55639217, то на выходе должно быть 5:563:921:7.
+
+*/
